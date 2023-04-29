0c200a5664f58c75a3cfa6f1052105962583b2d2
Roy-Kid <lijichen365@126.com>
2023-04-29 14:40:40 +0800

    add molcpp as submodule

--- cpp/include/example.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <xtensor/xarray.hpp>
-
-int int_add(int, int);
-double sum_of_sines(const xt::xarray<double>&);
---
+++ cpp/include/util.h
@@ -0,0 +1,5 @@
+#include <xtensor.hpp>
+#include <xtensor/xadapt.hpp>
+#include <pybind11/pybind11.h>
+#define FORCE_IMPORT_ARRAY
+#include <xtensor-python/pyarray.hpp>
